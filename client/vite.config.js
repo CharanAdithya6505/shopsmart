67f26d5a96305dfecd2d88d90446e7a726205ca0
CharanAdithya6505 <charanadithya6505@gmail.com>
2026-02-20 15:51:33 +0530

    feat: add GitHub Pages deployment workflow and configure Vite base path

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,15 +3,16 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/shopsmart/",
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5001",
-        changeOrigin: true,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "http://localhost:5001",
+  //       changeOrigin: true,
+  //     },
+  //   },
+  // },
   test: {
     globals: true,
     environment: "jsdom",
